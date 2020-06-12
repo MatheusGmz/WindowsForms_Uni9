04916d6ec15e441b69943674783975de4c99b5aa
MatheusGmz <64435454+MatheusGmz@users.noreply.github.com>
2020-06-12 15:52:56 -0300

    Add files via upload

---
+++ ItemManager.Crosscutting/NativeInjectorBootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using ItemManager.Infrastructure.Context;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ItemManager.Crosscutting
+{
+    public static class NativeInjectorBootstrapper
+    {
+        public static void RegisterServices (IServiceCollection services)
+        {
+            services.AddScoped<ManagerContext>();
+        }
+    }
+}
